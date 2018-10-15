fc044a74ae64279ad00d82eca2275681cc0be032
sojurner <pykim0591@gmail.com>
2018-10-14 20:27:38 -0600

    Destructure props and create new form for club search

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -1,15 +1,37 @@
 import React from 'react';
 
-export const FilterBar = ({ filterPlayersByCountry, searchByPlayer, handleChange, currentSearchValue, countries }) => {
+import './FilterBar.css';
 
-	return (
-		<div>
-			<select onChange={event => filterPlayersByCountry(event.target.value)}>
-				{countries}
-			</select>
-			<form onSubmit={searchByPlayer}>
-				<input name='searchInput' value={currentSearchValue} placeholder="Search by name" onChange={handleChange}/>
-			</form>
-		</div>
-	);
+export const FilterBar = ({
+  filterPlayersByCountry,
+  searchByPlayer,
+  handleChange,
+  currentSearchClub,
+  currentSearchName,
+  countries,
+  searchByClub
+}) => {
+  return (
+    <div>
+      <select onChange={event => filterPlayersByCountry(event.target.value)}>
+        {countries}
+      </select>
+      <form onSubmit={searchByPlayer}>
+        <input
+          name="searchedName"
+          value={currentSearchName}
+          placeholder="Search by name"
+          onChange={handleChange}
+        />
+      </form>
+      <form onSubmit={searchByClub}>
+        <input
+          onChange={handleChange}
+          name="searchedClub"
+          value={currentSearchClub}
+          placeholder="search by club"
+        />
+      </form>
+    </div>
+  );
 };
