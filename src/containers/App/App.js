ed1073a383a4f73dff01eb262075f0064a347089
sojurner <pykim0591@gmail.com>
2018-10-14 20:23:29 -0600

    Add classes to futbol title and div container

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Splash from '../../components/Splash/Splash';
 import Home from '../../components/Home/Home';
 import Dashboard from '../../containers/Dashboard/Dashboard';
 import Players from '../../containers/Players/Players';
 
-
 class App extends Component {
   render() {
     return (
-			<BrowserRouter>
-				<div>
-					<h1>futbol</h1>
-					<Route exact path='/' component={Splash}/>
-					<Route path='/home' component={Home}/>
-					<Route exact path='/home/dashboard' component={Dashboard}/>
-					<Route exact path='/home/players' component={Players}/>
-				</div>
-			</BrowserRouter>
-		);
+      <BrowserRouter>
+        <div className="body">
+          <h1 className="futbol-title">Fantasy Fútbol</h1>
+          <Route exact path="/" component={Splash} />
+          <Route path="/home" component={Home} />
+          <Route exact path="/home/dashboard" component={Dashboard} />
+          <Route exact path="/home/players" component={Players} />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
