4dcdc97a79bff85c593ff483a27738a5440c5a1b
sojurner <pykim0591@gmail.com>
2018-10-14 20:26:19 -0600

    Add class and import to Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+import './Home.css';
 
 const Home = () => {
-	return (
-		<nav>
-			<NavLink to='/home/dashboard'>Dashboard</NavLink>
-			<NavLink to='/home/players'>Players</NavLink>
-		</nav>
-	);
+  return (
+    <nav className="navigation">
+      <NavLink to="/home/dashboard" className="dashboard-tab nav-bar">
+        Dashboard
+      </NavLink>
+      <NavLink to="/home/players" className="players-tab nav-bar">
+        Players
+      </NavLink>
+    </nav>
+  );
 };
 
 export default Home;
