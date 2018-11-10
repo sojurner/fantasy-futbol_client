f46338a96a4444a45647efee7c5c3de5ba2a3960
sojurner <pykim0591@gmail.com>
2018-11-10 08:29:15 -0700

    Change to Polar chart

--- src/components/PlayerModal/PlayerModal.js
+++ src/components/PlayerModal/PlayerModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HorizontalBar } from 'react-chartjs-2';
+import { Polar } from 'react-chartjs-2';
 import './PlayerModal.css';
 
 export const PlayerModal = ({ stats, statType }) => {
@@ -10,24 +10,23 @@ export const PlayerModal = ({ stats, statType }) => {
     'rgb(187,187,187, 0.5)'
   ];
   const playerStats = stats.map((stat, index) => {
+    const backgroundColor = Object.keys(stat).map(
+      (stat, indice) => hexCodes[indice % 3]
+    );
     const data = {
       labels: Object.keys(stat),
+      display: false,
       datasets: [
         {
-          label: statType[index],
-          backgroundColor: `${hexCodes[index]}`,
+          backgroundColor,
           borderColor: 'rgba(179,181,198,1)',
-          pointBackgroundColor: 'rgba(179,181,198,1)',
-          pointBorderColor: '#fff',
-          pointHoverBackgroundColor: '#fff',
+          pointHoverBackgroundColor: '#ffffa',
           pointHoverBorderColor: 'rgba(179,181,198,1)',
           data: Object.values(stat)
         }
       ]
     };
-    return (
-      <HorizontalBar data={data} key={`${statType}-${index}`} height="215px" />
-    );
+    return <Polar data={data} key={`${statType}-${index}`} height="215px" />;
   });
   return <div>{playerStats}</div>;
 };
