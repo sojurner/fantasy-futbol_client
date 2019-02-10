4b8ff144f66b1f269cd6c9e4573f31195b72d427
Paul Kim <pykim0591@gmail.com>
2019-02-10 06:30:29 -0700

    Remove loading gif and add fallback image

--- src/components/PlayerRow/PlayerRow.js
+++ src/components/PlayerRow/PlayerRow.js
@@ -16,7 +16,13 @@ export const PlayerRow = ({
   return (
     <tr data-id={id} className={Name} onClick={onOpenModal.bind(null, id)}>
       <td>
-        <img src={Photo} height="35" className="photo" alt="alt_text" />
+        <img
+          src={Photo}
+          onError={e => {
+            e.target.onerror = null;
+            e.target.src =
+              'https://www.referee.com/wp-content/uploads/2012/10/FIFA.png';
+          }}
       </td>
       <td>{Name}</td>
       <td>{Nationality}</td>
